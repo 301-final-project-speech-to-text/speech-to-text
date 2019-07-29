6cc870c3d65bd977816b57860498a9e53734e103
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-07-29 15:06:31 -0700

    fixing schema bug, adding insert

--- data/schema.sql
+++ data/schema.sql
@@ -1,20 +1,4 @@
-DROP TABLE IF EXISTS lang ;
-DROP TABLE IF EXISTS trans ;
-
-CREATE TABLE lang (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255)
-);
-CREATE TABLE trans (
-  id SERIAL PRIMARY KEY,
-  string TEXT,
-  translation TEXT,
-  lang_name_id INTEGER NOT NULL,
-  lang_trans_name_id INTEGER  NOT NULL,
-  FOREIGN KEY (lang_name_id) REFERENCES lang (id),
-  FOREIGN KEY (lang_trans_name_id) REFERENCES lang (id)
-);
-
+git reset --hard
 
 INSERT INTO lang (name) 
 VALUES ('English'),
