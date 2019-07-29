98d701bcdc5c7c62ee3a2f36ee3d52f7c6079786
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-07-29 15:04:47 -0700

    fixing schema bug, adding insert

--- data/schema.sql
+++ data/schema.sql
@@ -11,9 +11,26 @@ CREATE TABLE trans (
   translation TEXT,
   lang_name_id INTEGER NOT NULL,
   lang_trans_name_id INTEGER  NOT NULL,
-  FOREIGN KEY (lang_name_id, lang_trans_name_id) REFERENCES lang (id)
+  FOREIGN KEY (lang_name_id) REFERENCES lang (id),
+  FOREIGN KEY (lang_trans_name_id) REFERENCES lang (id)
 );
 
 
-INSERT INTO lang (name) VALUES ('English');
-INSERT INTO lang (name) VALUES ('Russian');
+INSERT INTO lang (name) 
+VALUES ('English'),
+('Chinese'),
+('French'),
+('German'),
+('Italian'),
+('Japanese'),
+('Korean'),
+('Russian'),
+('Spanish'),
+('Thai'),
+('Vietnamese');
+
+INSERT INTO trans (string, translation, lang_name_id, lang_trans_name_id ) 
+VALUES ('Hello', 'Привет', '1', '8'),
+('Привет', 'Hola', '8', '9')
+
+
