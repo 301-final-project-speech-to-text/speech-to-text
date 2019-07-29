6b56fff8459b05e7c1b524399ca029767b38ad2b
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-07-29 15:17:49 -0700

    another typo

--- data/schema.sql
+++ data/schema.sql
@@ -32,23 +32,3 @@ VALUES ('English'),
 INSERT INTO trans (string, translation, lang_name_id, lang_trans_name_id ) 
 VALUES ('Hello', 'Привет', '1', '8'),
 ('Привет', 'Hola', '8', '9')
-
-
-INSERT INTO lang (name) 
-VALUES ('English'),
-('Chinese'),
-('French'),
-('German'),
-('Italian'),
-('Japanese'),
-('Korean'),
-('Russian'),
-('Spanish'),
-('Thai'),
-('Vietnamese');
-
-INSERT INTO trans (string, translation, lang_name_id, lang_trans_name_id ) 
-VALUES ('Hello', 'Привет', '1', '8'),
-('Привет', 'Hola', '8', '9')
-
-
