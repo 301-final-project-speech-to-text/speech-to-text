3cd7996888b51b42b837d040838db8623cd91f20
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-07-29 16:06:54 -0700

    update gitignore

--- data/schema.sql
+++ data/schema.sql
@@ -31,4 +31,4 @@ VALUES ('English'),
 
 INSERT INTO trans (string, translation, lang_name_id, lang_trans_name_id ) 
 VALUES ('Hello', 'Привет', '1', '8'),
-('Привет', 'Hola', '8', '9')
+('Привет', 'Hola', '8', '9');
